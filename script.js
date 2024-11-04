4b968c095c97524976ed2322b6234663e47f5261
manoarvind27@gmail.com <manoarvind27@gmail.com>
2024-11-04 17:13:18 +0530

    updated

--- script.js
+++ script.js
@@ -183,3 +183,32 @@ setInterval(function(){
 });
 
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var Acc = document.getElementsByClassName("Accordion");
+var i;
+
+for (i = 0; i < Acc.length; i++) {
+  Acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var paneL = this.nextElementSibling;
+    if (paneL.style.display === "block") {
+      paneL.style.display = "none";
+    } else {
+      paneL.style.display = "block";
+    }
+  });
+}
